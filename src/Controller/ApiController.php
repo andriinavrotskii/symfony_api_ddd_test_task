fcf186236974842f1789db4f1d932dd4de1c4d33
Andrii Navrotskiy <andrii.navrotskiy@uvoteam.com>
2018-11-29 00:25:43 +0200

    first method

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiController extends FOSRestController
+{
+    /**
+     * @Rest\Get("/first")
+     * @param Request $request
+     * @return View
+     */
+    public function firstAction(Request $request)
+    {
+        return View::create([
+            'message' => 'hello get'
+        ], Response::HTTP_OK);
+    }
+}
